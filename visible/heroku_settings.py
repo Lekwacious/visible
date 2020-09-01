8efc3c01d0db70ba4e8ac74ce010d3f6b31b70b9
Lekwacious <emelekwa@gmail.com>
2020-09-01 13:43:27 +0100

    heroku

---
+++ visible/heroku_settings.py
@@ -0,0 +1,17 @@
+from settings import *
+
+## settings.py
+import dj_database_url
+
+# set default value
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_name',
+    }
+}
+
+# if environment variable present, it must be Heroku
+if 'DATABASE_URL' in os.environ:
+    # replace default with parsed value
+    DATABASES['default'] = dj_database_url.config(ssl_require=True)
