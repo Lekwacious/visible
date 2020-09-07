557e5cd7b664a3c8384de01a0be5b01720b9119d
Lekwacious <emelekwa@gmail.com>
2020-09-07 14:53:11 +0100

    update

--- account/views.py
+++ account/views.py
@@ -17,8 +17,8 @@ def dashboard(request):
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
+        # first_name = request.POST['first_name']
+        # last_name = request.POST['last_name']
         email = request.POST['email']
         password = request.POST['password']
         password_repeat = request.POST['password_repeat']
@@ -31,8 +31,7 @@ def register(request):
                 messages.info(request, 'Email already exist')
                 return render(request, 'account/register.html')
             else:
-                user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                                email=email, password=password)
+                user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
                # Profile.objects.create(user=user)
 
