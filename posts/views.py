51a0c709a5d9ce49b05e978bac089dbb5179feb3
Lekwacious <emelekwa@gmail.com>
2020-09-07 13:56:18 +0100

    update

--- posts/views.py
+++ posts/views.py
@@ -10,7 +10,7 @@ from django.contrib import messages
 
 @login_required
 def post_comment_create_and_list_view(request):
-    qs = Post.objects.all()
+    qs = Post.objects.filter(request.profile.get_friends)
    # profile = Profile.objects.get(user=request.user)
 
     p_form = PostModelForm()
